43752bda95dd5ba6d0169e3346243a9c149addad
ZSchlesinger <zach.schlesinger@gmail.com>
2017-09-08 22:22:58 -0400

    adf

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-@app.route("/sms", methods =['GET', 'POST'])
-def sms_reply():
-    """Respond to incoming calls with simple text message."""
-
-    body = request.values.get('Body', None)
-
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    if body == 'hello':
-        resp.message("hi!")
-    elif body == 'bye':
-        resp.message("Goodbye")
-
-    return str(resp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
