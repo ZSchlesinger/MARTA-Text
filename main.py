3ce3851c0988e8fcc24a3962eda0c7f2968fbd20
Jason Wu <jasonwupilly1997@gmail.com>
2017-09-09 15:06:22 -0400

    its back

--- main.py
+++ main.py
@@ -51,8 +51,7 @@ def send_sms():
             from_="+14703001965",
             body='Message from MARTA:' + request.form.get('data')
             )
-    # return redirect('/dashboard')
-    return "asdf"
+    return redirect('/dashboard')
 
 @app.route("/sms", methods =['GET', 'POST'])
 def sms_reply():
